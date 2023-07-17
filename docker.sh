762d14c03904568d7c8d87f5a68c76a227d42659
Alex Korotky <alexander.korotkyi@gmail.com>
2023-07-16 22:07:37 -0400

    update app properties & bash scripts

--- docker.sh
+++ docker.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-# name and tag of the docker image
-IMAGE_NAME_TAG="eurekaserver:0.0.1-SNAPSHOT"
+# build a Docker image
+mvn clean spring-boot:build-image -D skipTests
 
-# check if the image already exists
-if [ "$(docker images -q ${IMAGE_NAME_TAG} 2>/dev/null)" = "" ]; then
-  echo "Image does not exist. Building now..."
-
-  # build the Docker image
-  mvn spring-boot:build-image
-else
-  echo "Image already exists. No need to build."
-fi
-
-# start the docker container
-docker run --rm --name wf-eureka-server -p 8761:8761 ${IMAGE_NAME_TAG}
+# push the image to docker hub repository
+docker tag eurekaserver:0.0.1-SNAPSHOT akorotky/webforum:eurekaserver
+docker push akorotky/webforum:eurekaserver
+.
